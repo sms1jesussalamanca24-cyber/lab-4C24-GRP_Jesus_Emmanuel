70c5da6b9fcdf8a4d798ecf6864c7b0e198f3041
Jesus Salamanca <sms1jesussalamanca24@gmail.com>
2026-09-04 22:40:30 -0500

    Update StudentController

--- src/StudentController.java
+++ src/StudentController.java
@@ -1,5 +1,11 @@
 public class StudentController {
     
+    private StudentModel model;
+    
+    public StudentController(StudentModel model) {
+        this.model = model;
+    }
+    
     public void getStudents() {
         System.out.println("Listando estudiantes...");
     }
@@ -10,5 +16,9 @@ public class StudentController {
     
     public void deleteStudent(int id) {
         System.out.println("Eliminando estudiante con ID: " + id);
+    }
+    
+    public void updateStudent(int id, String name) {
+        System.out.println("Actualizando estudiante ID: " + id + " - Nombre: " + name);
     }
 }
