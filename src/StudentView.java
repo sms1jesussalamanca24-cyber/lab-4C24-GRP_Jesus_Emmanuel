8df80a2c050254a165707376460fa15eaad0def9
Jesus Salamanca <sms1jesussalamanca24@gmail.com>
2026-09-04 22:57:06 -0500

    Add StudentView

---
+++ src/StudentView.java
@@ -0,0 +1,11 @@
+public class StudentView {
+    
+    public void printStudentDetails(String name, int id) {
+        System.out.println("Estudiante: " + name);
+        System.out.println("ID: " + id);
+    }
+    
+    public void printAllStudents() {
+        System.out.println("Mostrando todos los estudiantes...");
+    }
+}
