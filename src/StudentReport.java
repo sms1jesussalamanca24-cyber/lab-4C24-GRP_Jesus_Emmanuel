41b1db6d3f35a3a2fa071f7129421d6bf6a8c871
Emmanuel Salas <emmanuel.salas@tecsup.edu.pe.com>
2026-09-04 23:11:40 -0500

    Add StudentReport

---
+++ src/StudentReport.java
@@ -0,0 +1,10 @@
+public class StudentReport {
+    
+    public void generateReport() {
+        System.out.println("Generando reporte de estudiantes...");
+    }
+    
+    public void generateReportById(int id) {
+        System.out.println("Generando reporte del estudiante ID: " + id);
+    }
+}
